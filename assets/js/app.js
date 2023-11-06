4aac281a1478a73dfa91afabff6de6689d3305d3
Huseyn-Ismayilov <info.cyweb@gmail.com>
2023-11-07 00:54:28 +0400

    #

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,3 +42,38 @@ $('.select_language .btn').click(function () {
 	$(this).next().slideToggle()
 });
 
+
+
+
+const rangeMain = document.querySelectorAll(".rangeSlider");
+function customSlider(e) {
+	const slider = e.currentTarget;
+	const thumb = slider.closest(".rangeSlider").querySelector('.rsThumb');
+	const tooltip = slider.closest(".rangeSlider").querySelector('.rsTooltip');
+	const progress = slider.closest(".rangeSlider").querySelector('.rsProgress');
+	const maxVal = slider.getAttribute("max");
+	const val = (slider.value / maxVal) * 100 + "%";
+	tooltip.innerHTML = slider.value;
+	progress.style.width = val;
+	thumb.style.left = val;
+}
+for (let i = 0; i < rangeMain.length; i++) {
+	const slider = rangeMain[i].querySelector(".rsBar");
+	slider.addEventListener("input", customSlider);
+	slider.addEventListener("touchstart", customSlider);
+	slider.addEventListener("touchmove", customSlider);
+	const e = new Event('touchstart');
+	slider.dispatchEvent(e);
+};
+// Range slider error function
+function rangeSlider_error(id) {
+	if ($(id).val() < 1) {
+		$(id).parents('.form-group').addClass('red');
+		$(id).parents('.form-group').find('.errorText').removeClass('hide');
+		return false;
+	} else {
+		$(id).parents('.form-group').removeClass('red');
+		$(id).parents('.form-group').find('.errorText').addClass('hide');
+		return true;
+	}
+}
